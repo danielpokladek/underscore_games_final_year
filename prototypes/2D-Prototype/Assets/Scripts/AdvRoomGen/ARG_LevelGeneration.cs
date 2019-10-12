593880181e19a4f56de11cd3cfee16f2a0c07018
Daniel Pokladek <danielpokladek@gmail.com>
2019-10-12 22:38:09 +0100

    Update ARG_LevelGeneration.cs
    Further work on the advanced room generation.

--- prototypes/2D-Prototype/Assets/Scripts/AdvRoomGen/ARG_LevelGeneration.cs
+++ prototypes/2D-Prototype/Assets/Scripts/AdvRoomGen/ARG_LevelGeneration.cs
@@ -8,6 +8,12 @@ using UnityEngine;
  * Description:
  * Not done.
  * 
+ * Room Types List:
+ *  -   0: Normal Room
+ *  -   1: (starting) Entrance Room
+ *  -   2: (boss) Final Room
+ *  -   3: Shop Room
+ * 
  */
 
 public class ARG_LevelGeneration : MonoBehaviour
@@ -41,6 +47,53 @@ public class ARG_LevelGeneration : MonoBehaviour
     }
 
     private void CreateRooms()
+    {
+        rooms = new ARG_Room[gridSizeX * 2, gridSizeY * 2];
+        rooms[gridSizeX, gridSizeY] = new ARG_Room(Vector2.zero, 1);
+        takenPositions.Insert(0, Vector2.zero);
+        Vector2 checkPos = Vector2.zero;
+
+        // Magic Numbers used in branching the rooms.
+        float randomCompare = 0.2f;
+        float randomCompareStart = 0.2f;
+        float randomCompareEnd = 0.01f;
+
+        // Add Rooms
+        for (int i = 0; i < numberOfRooms - 1; i++)
+        {
+            // This generator will determine wether the rooms should be clumped together, or branched out.
+            // The randomCompare also will make it, so that the more rooms there are, the less chance for the level to branch out.
+            // Changing the magic numbers, will allow for more/less clumped levels.
+
+            float randomPerc = ((float)i) / (((float)numberOfRooms - 1));
+            randomCompare = Mathf.Lerp(randomCompareStart, randomCompareEnd, randomPerc);
+
+            // Grab New Room Position
+            checkPos = NewPosition();
+
+            // Test New Position
+            if (NumberOfNeighbours(checkPos, takenPositions) > 1 && Random.value > randomCompare)
+            {
+                // If we do want to branch out, the generator will look for a new position with 1 neighbour that connects it.
+                int iterations = 0;
+                do
+                {
+                    checkPos = SelectiveNewPosition();
+                    iterations++;
+                } while (NumberOfNeigbours(checkPos, takenPositions) > 1 && iterations < 100);
+
+                if (iterations >= 50)
+                    Debug.LogError("GEN_ERROR: Could not create with fewer neighbours than: " + NumberOfNeighbours(checkPos, takenPositions));
+            }
+
+            // Finalize Position, add to list and calculate the offset.
+            // Once it is finalized, construct the new room position with roomType '0' (normal room - check above for room types)
+            rooms[(int)checkPos.x + gridSizeX, (int)checkPos.y + gridSizeY] = new ARG_Room(checkPos, 0);
+            takenPositions.Insert(0, checkPos);
+        }
+    }
+
+    private Vector2 NewPosition()
     {
         throw new System.NotImplementedException();
     }
