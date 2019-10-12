5cb9e504735f0e4248693d5196a8b00df8f3ce6b
Daniel Pokladek <danielpokladek@gmail.com>
2019-10-12 22:01:16 +0100

    Working on more advanced level generation

---
+++ prototypes/2D-Prototype/Assets/Scripts/AdvRoomGen/ARG_LevelGeneration.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Level Generation Script
+ * 
+ * Description:
+ * Not done.
+ * 
+ */
+
+public class ARG_LevelGeneration : MonoBehaviour
+{
+    public GameObject roomWhiteObj;
+
+    Vector2 worldSize = new Vector2(4, 4);                  // This is the size of the world (in room lenghts),
+                                                            //  but this in halfs, so (4,4) will create an 8x8 grid.
+
+    ARG_Room[,] rooms;                                      // Reference to a 2D array of rooms in the level.
+
+    List<Vector2> takenPositions = new List<Vector2>();     // Although we already have an array of rooms,
+                                                            //  a List<> allows to find rooms using 'contains' method.
+
+    private int gridSizeX, gridSizeY;
+    private int numberOfRooms = 20;
+
+    private void Start()
+    {
+        if (numberOfRooms >= (worldSize.x * 2) * (worldSize.y * 2))
+        {
+            numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
+        }
+
+        gridSizeX = Mathf.RoundToInt(worldSize.x);
+        gridSizeY = Mathf.RoundToInt(worldSize.y);
+
+        CreateRooms();
+        SetRoomDoors();
+        DrawMap();
+    }
+
+    private void CreateRooms()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void SetRoomDoors()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void DrawMap()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ prototypes/2D-Prototype/Assets/Scripts/AdvRoomGen/ARG_Room.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* !!THIS NEEDS RE-WRITING FOR BETTER UNDERSTANDING!!
+ * 
+ * ROOM  Script, contains data about the room
+ *  
+ * The Vector2 stores the current room position on the grid,
+ * it's worth noting that not all grid slots are populated with rooms.
+ * 
+ * Room type determines the type of room this is, for example Entrance, Boss, Shop, etc.
+ * 
+ * Finally the bool determines which way the doors are facing.
+ * This can probably be done with integer, or a char if required later.
+ * Currently works fine for the rooms that we have.
+ * 
+ */
+
+public class ARG_Room
+{
+                                                            // :-- EXPAND THIS INFORMATION LATER ON --:
+                                                            //  
+    public Vector2 gridPos;                                 // Room position on the grid.
+    public int roomType;                                    // Room type (entrance, shop, boss, etc.)
+    public bool doorTop, doorBot, doorLeft, doorRight;      // Which direction has doors (top, down, etc.)
+
+    public ARG_Room(Vector2 _gridPos, int _roomType)
+    {
+        gridPos     = _gridPos;
+        roomType    = _roomType;
+    }
+}
