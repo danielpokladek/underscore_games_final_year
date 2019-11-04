546ff9bead8c7e9c3fdd02a16c1fe0686471bf1d
Daniel Pokladek <danielpokladek@gmail.com>
2019-11-04 00:54:23 +0000

    Room Generation & Spawning The Boss
    Renamed the Unity folders, as its no longer proof of concept but the main project.
    
    The generator will now randomly select a room, in which the boss will spawn at night. At the moment the generator will only spawn a boss icon, but for playtesting we can just load the other scene for the players.
    
    Slightly adjusted the rooms created by Len, so that they line up with the other rooms. Added Len's rooms to the room generator, so that we can use those levels for playtesting.

---
+++ Unity Projects/2DRoguelite/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : Bullet
+{   
+    private void Start()
+    {
+        Destroy(gameObject, 3.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            EnemyController enemyController = other.GetComponent<EnemyController>();
+            enemyController.TakeDamage(bulletDamage);
+
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.4f);
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            playerController.TakeDamage(bulletDamage);
+
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.4f);
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("BossHitPoint"))
+        {
+            other.transform.parent.GetComponent<BossController>().DamageBoss(bulletDamage);
+
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.4f);
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("BossProjectile"))
+        {
+            other.gameObject.GetComponent<BossBullet>().DamageBullet(bulletDamage);
+
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.4f);
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.4f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity Projects/2DRoguelite/Assets/Scripts/Room Logic/RoomManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemySpawners;
+    [SerializeField] private GameObject boss;
+
+    private bool enemiesSpawned = false;
+
+    public void SpawnPlayer()
+    {
+        Instantiate(GameManager.instance.playerPrefab, transform.position, Quaternion.identity);
+    }
+
+    public void SpawnBoss(GameObject bossIcon)
+    {
+        Instantiate(bossIcon, transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player") || enemiesSpawned) return;
+        
+        foreach (GameObject spawner in enemySpawners)
+        {
+            EnemySpawnPoint spawnPoint = spawner.GetComponent<EnemySpawnPoint>();
+            spawnPoint.SpawnEnemy();
+        }
+
+        enemiesSpawned = true;
+    }
+}
