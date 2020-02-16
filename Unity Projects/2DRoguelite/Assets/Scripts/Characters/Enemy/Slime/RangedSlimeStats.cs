eb38ce0232bbe00acc6500c589c59afa2dafca75
Daniel Pokladek <danielpokladek@gmail.com>
2020-02-16 17:15:24 +0000

    Added colliders to enemies, so they won't stay on top of eachother now.

--- Unity Projects/2DRoguelite/Assets/Scripts/Characters/Enemy/Slime/RangedSlimeStats.cs
+++ Unity Projects/2DRoguelite/Assets/Scripts/Characters/Enemy/Slime/RangedSlimeStats.cs
@@ -6,8 +6,13 @@ public class RangedSlimeStats : EnemyStats
 
     override protected void CharacterDeath()
     {
-        Instantiate(meleeSlime, transform.position + new Vector3(1.5f, 1.5f, 0.0f), Quaternion.identity);
-        Instantiate(meleeSlime, transform.position + new Vector3(-1.5f, -1.5f, 0.0f), Quaternion.identity);
+        float rand1 = Random.Range(0.0f, 1.5f);
+        float rand2 = Random.Range(0.0f, 1.5f);
+        float rand3 = Random.Range(0.0f, 1.5f);
+        float rand4 = Random.Range(0.0f, 1.5f);
+
+        Instantiate(meleeSlime, transform.position + new Vector3(rand1, rand2, 0.0f), Quaternion.identity);
+        Instantiate(meleeSlime, transform.position + new Vector3(rand3, rand4, 0.0f), Quaternion.identity);
 
         base.CharacterDeath();
     }
