a1e806bef43d52e81a5324c9fd417907d62b8992
Daniel Pokladek <danielpokladek@users.noreply.github.com>
2020-02-17 12:50:39 +0000

    Added new enemy for split slimes

--- Unity Projects/2DRoguelite/Assets/Scripts/Characters/Enemy/Slime/RangedSlimeStats.cs
+++ Unity Projects/2DRoguelite/Assets/Scripts/Characters/Enemy/Slime/RangedSlimeStats.cs
@@ -11,11 +11,8 @@ public class RangedSlimeStats : EnemyStats
         float rand3 = Random.Range(0.0f, 1.5f);
         float rand4 = Random.Range(0.0f, 1.5f);
 
-        GameObject slime1 = Instantiate(meleeSlime, transform.position + new Vector3(rand1, rand2, 0.0f), Quaternion.identity);
-        GameObject slime2 = Instantiate(meleeSlime, transform.position + new Vector3(rand3, rand4, 0.0f), Quaternion.identity);
-
-        slime1.transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
-        slime1.transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+        Instantiate(meleeSlime, transform.position + new Vector3(rand1, rand2, 0.0f), Quaternion.identity);
+        Instantiate(meleeSlime, transform.position + new Vector3(rand3, rand4, 0.0f), Quaternion.identity);
 
         base.CharacterDeath();
     }
