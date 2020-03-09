c57d26279f3b4811f6e164e49fb50c3751fec35d
Daniel Pokladek <danielpokladek@gmail.com>
2020-03-09 22:27:52 +0000

    v0.4.7
    - Updated portal animation
    - Changed inner part of the portal
    - Portal now opens when enough enemies are killed
    - Minor tweaks to improve experience.

--- Unity Projects/2DRoguelite/Assets/Scripts/Room Generation/BossPortal.cs
+++ Unity Projects/2DRoguelite/Assets/Scripts/Room Generation/BossPortal.cs
@@ -5,18 +5,36 @@ using TMPro;
 
 public class BossPortal : MonoBehaviour
 {
-    [SerializeField] private TMP_Text portalCounter;
+    [SerializeField] private TMP_Text soulsText;
+    [SerializeField] private Material innerMaterial;
+    [SerializeField] private Animator portalAnimator;
 
     private bool portalEnabled = false;
+    private bool showInner;
+
+    private float dissolveAmount;
 
     private void Start()
     {
         LevelManager.instance.portalChargedCallback += EnablePortal;
+        portalAnimator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        portalCounter.text = LevelManager.instance.killsRequired.ToString("00");
+        soulsText.text = LevelManager.instance.killsRequired.ToString("00");
+
+        if (showInner && dissolveAmount < 1)
+        {
+            dissolveAmount += Time.deltaTime * 1.5f;
+            innerMaterial.SetFloat("_DissolveAmount", dissolveAmount);
+        }
+    }
+
+    public void ShowInnerPortal()
+    {
+        dissolveAmount = 0;
+        showInner = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -31,5 +49,7 @@ public class BossPortal : MonoBehaviour
     private void EnablePortal()
     {
         portalEnabled = true;
+        soulsText.enabled = false;
+        portalAnimator.SetTrigger("_OpenPortal");
     }
 }
